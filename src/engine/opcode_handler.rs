9353cc32341bc041b880dccca0babeffcf7fefe4
Sophie Holman <siaphix.siaphix@gmail.com>
2025-11-09 14:25:14 +0000

    handler input info skeleton

--- src/engine/opcode_handler.rs
+++ src/engine/opcode_handler.rs
@@ -1,14 +1,22 @@
+use std::array::from_fn;
+
+struct HandlerInputInfo
+{
+    opcode: u8,
+    params: Vec<u8>,
+}
+
 #[derive(Clone, Copy)]
 struct HandlerInfo
 {
     opcode: u8,
     param_count: u8,
-    handler: fn(),
+    handler: fn(HandlerInputInfo),
 }
 
 const HANDLERS: [HandlerInfo; 256] = [HandlerInfo {opcode: 0, param_count: 0, handler: unimplemented_handler}; 256];
 
-fn unimplemented_handler()
+fn unimplemented_handler(input: HandlerInputInfo)
 {
     panic!("Opcode not implemented");
 }
