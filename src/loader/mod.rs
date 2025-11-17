736f8256e2451c7f520e4519123229f0f85bd699
Sophie Holman <siaphix.siaphix@gmail.com>
2025-11-17 21:41:27 +0000

    Loader static initialisation

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -1,5 +1,6 @@
 use crate::engine::opcodes::Opcode;
 
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub enum Directive
 {
     Start,
@@ -7,27 +8,123 @@ pub enum Directive
     MaxLocals(u16),
 }
 
-pub struct Loader<'a>
+pub struct Loader
 {
-    runnables: Vec<Runnable<'a>>,
+    runnables: Vec<Runnable>,
 }
 
 // This is a temporary solution that just statically loads the
 // entire file at once.
 // In the future this will happen dynamically where required.
-impl<'a> Loader<'a>
+impl Loader
+{
+    pub fn from_file(filename: &str) -> Option<Self>
+    {
+        // Again there is definitely a better way of doing this that doesn't
+        // involve spamming mutable variables everywhere
+        // But this entire system is going to get rewritten as some point
+        // and I just want it working right now.
+
+        let contents = std::fs::read(filename).ok()?;
+        let mut runnables: Vec<Runnable> = vec![];
+
+        let mut remaining: &[u8] = &contents;
+        while let [_, ..] = remaining
+        {
+            let (runnable, rem) = Runnable::from_bytes(remaining)?;
+            runnables.push(runnable);
+            remaining = rem;
+        }
+
+
+        Some(Self {
+            runnables
+        })
+    }
+
+    pub fn get_entry_point(&self) -> Option<&Runnable>
+    {
+        self.runnables.iter().find(|x| x.directives.contains(&Directive::Start))
+    }
+}
+
+pub struct Runnable
+{
+    maxstack: usize,
+    maxlocals: usize,
+    directives: Vec<Directive>,
+    bytecode: Vec<u8>,
+}
+
+impl Runnable
 {
     const DIRECTIVE_OPCODE: u8 = Opcode::Directive as u8;
 
-    pub fn from_file(filename: &str) -> Option<Self>
+    pub fn from_bytes(input: &[u8]) -> Option<(Self, &[u8])>
     {
-        let contents = std::fs::read(filename).ok()?;
-
         // Read off directives for function metadata
+        let mut directives: Vec<Directive> = vec![];
 
+        // There might be a way of doing this with less mutability
+        let mut remaining = input;
+        loop
+        {
+            match Self::match_off(remaining)
+            {
+                Ok((dir, rem)) => {
+                    directives.push(dir);
+                    remaining = rem;
+                }
+                Err(Some(code)) => {
+                    let bytecode = Vec::from_iter(code.iter().copied());
+                    let rem = &remaining[bytecode.len()..];
+                    return Some(
+                        (
+                            Self::from_parsed_data(directives, bytecode)?,
+                            rem
+                        )
+                    )
+                }
+                Err(None) => {
+                    return None;
+                }
+            }
+        }
     }
 
-    fn match_off(input: &'a [u8]) -> Result<(Directive, &'a [u8]), Option<&'a [u8]>>
+    fn from_parsed_data(directives: Vec<Directive>, bytecode: Vec<u8>) -> Option<Self>
+    {
+        // This right now is fairly shit but I just want it working
+        // The entire loader is going to be changed at some point down the line anyway
+        let mut max_stack: Option<usize> = None;
+        let mut max_locals: Option<usize> = None;
+
+        // This is all just soo bad
+        let (required, directives) = directives.iter().partition(|x| matches!(x, Directive::MaxStack(_) | Directive::MaxLocals(_)));
+
+        for directive in required
+        {
+            match directive
+            {
+                Directive::MaxStack(x) => {
+                    max_stack.replace(x.into()).map_or(Some(()), |_| None)?;
+                }
+                Directive::MaxLocals(x) => {
+                    max_locals.replace(x.into()).map_or(Some(()), |_| None)?;
+                }
+                _ => unreachable!()
+            }
+        }
+
+        Some(Self {
+            maxstack: max_stack?,
+            maxlocals: max_locals?,
+            directives,
+            bytecode: bytecode,
+        })
+    }
+
+    fn match_off(input: &[u8]) -> Result<(Directive, &[u8]), Option<&[u8]>>
     {
         match input
         {
@@ -36,21 +133,5 @@ impl<'a> Loader<'a>
             [Self::DIRECTIVE_OPCODE, 2, b1, b2, rem @ ..] => Ok((Directive::MaxLocals(u16::from_le_bytes([*b1, *b2])), rem)),
             code => Err(Some(code))
         }
-    }
-}
-
-pub struct Runnable<'a>
-{
-    maxstack: usize,
-    maxlocals: usize,
-    directives: Vec<Directive>,
-    bytecode: &'a [u8]
-}
-
-impl<'a> Runnable<'a>
-{
-    pub fn new() -> Self
-    {
-
     }
 }
