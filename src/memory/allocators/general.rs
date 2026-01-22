0b3f80dd3a11586784cd8bb4f909525e40d499fc
Sophie Holman <siaphix.siaphix@gmail.com>
2026-01-22 15:10:00 +0000

    allowed general allocator to work embedded

--- src/memory/allocators/general.rs
+++ src/memory/allocators/general.rs
@@ -5,13 +5,31 @@ use std::{alloc::{Layout, dealloc}, ptr::NonNull};
 pub struct GeneralAllocator
 {
     base: NonNull<u8>,
-    layout: Layout
+    capacity: usize,
+    layout: Option<Layout>
 }
 
 impl Drop for GeneralAllocator
 {
     fn drop(&mut self)
     {
-        unsafe { dealloc(self.base.as_ptr(), self.layout); }
+        if let Some(layout) = self.layout { unsafe { dealloc(self.base.as_ptr(), layout); } }
+    }
+}
+
+impl GeneralAllocator
+{
+    pub fn with_capacity()
+    {
+
+    }
+
+    pub fn with_existing_allocation(base: NonNull<u8>, capacity: usize) -> Self
+    {
+        Self {
+            base,
+            capacity,
+            layout: None,
+        }
     }
 }
