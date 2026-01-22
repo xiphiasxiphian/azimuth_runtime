2366dba58086c185585c4e24a0f09952f43082c8
Sophie Holman <siaphix.siaphix@gmail.com>
2026-01-22 14:49:55 +0000

    arena allocator tests

--- src/memory/allocators/arena.rs
+++ src/memory/allocators/arena.rs
@@ -1,5 +1,7 @@
 use std::{alloc::{Layout, alloc, dealloc}, ptr::NonNull};
 
+use crate::memory::allocators::ALIGNMENT;
+
 pub struct ArenaAllocator
 {
     base: NonNull<u8>,
@@ -43,7 +45,9 @@ impl ArenaAllocator
 
     pub fn alloc<T>(&mut self, value: T) -> Option<NonNull<T>>
     {
-        (size_of_val(&value) + self.head_offset <= self.capacity).then(|| {
+        let adjusted_size = size_of_val(&value).next_multiple_of(ALIGNMENT);
+
+        (adjusted_size + self.head_offset <= self.capacity).then(|| {
             let ptr: NonNull<T> = unsafe { self.base.byte_add(self.head_offset) }.cast();
             self.head_offset += size_of_val(&value);
 
@@ -55,5 +59,92 @@ impl ArenaAllocator
     pub fn release_all(&mut self)
     {
         self.head_offset = 0;
+    }
+}
+
+#[cfg(test)]
+mod arena_tests
+{
+    use super::*;
+
+    struct TestingData
+    {
+        number: i32,
+        character: char,
+        boolean: bool,
+        text: &'static str,
+    }
+
+    #[test]
+    fn arena_created()
+    {
+        let _ = ArenaAllocator::with_capacity(1024).unwrap();
+    }
+
+    #[test]
+    fn single_allocation()
+    {
+        let mut arena = ArenaAllocator::with_capacity(1024).unwrap();
+        let ptr = arena.alloc(TestingData { number: 1, character: 'a', boolean: false, text: "Hello!" }).unwrap();
+
+        unsafe {
+            assert_eq!(ptr.read().boolean, false);
+            assert_eq!(ptr.read().number, 1);
+            assert_eq!(ptr.read().character, 'a');
+            assert_eq!(ptr.read().text, "Hello!");
+        }
+    }
+
+    #[test]
+    fn multi_allocation()
+    {
+        let mut arena = ArenaAllocator::with_capacity(1024).unwrap();
+        let ptrs: Vec<_> = (0..10).map(|x| arena.alloc(x).unwrap()).collect();
+
+        for (i, ptr) in ptrs.iter().enumerate()
+        {
+            assert_eq!(unsafe { ptr.read() }, i);
+        }
+    }
+
+    #[test]
+    fn multi_sized_allocations()
+    {
+        let mut arena = ArenaAllocator::with_capacity(1024).unwrap();
+
+        let integer = arena.alloc(5).unwrap();
+        let boolean = arena.alloc(true).unwrap();
+        let string = arena.alloc("Hello World!").unwrap();
+        let character = arena.alloc('b').unwrap();
+        let testing_data = arena.alloc(TestingData { number: 1, character: 'a', boolean: false, text: "Hello!" }).unwrap();
+
+        unsafe
+        {
+            assert_eq!(integer.read(), 5);
+            assert_eq!(boolean.read(), true);
+            assert_eq!(string.read(), "Hello World!");
+            assert_eq!(character.read(), 'b');
+
+            assert_eq!(testing_data.read().boolean, false);
+            assert_eq!(testing_data.read().number, 1);
+            assert_eq!(testing_data.read().character, 'a');
+            assert_eq!(testing_data.read().text, "Hello!");
+        }
+    }
+
+    #[test]
+    fn deallocation()
+    {
+        let mut arena = ArenaAllocator::with_capacity(1024).unwrap();
+
+        let ptr1 = arena.alloc("Hello!").unwrap();
+        assert_eq!(unsafe { ptr1.read() }, "Hello!");
+
+        arena.release_all();
+
+        let ptr2 = arena.alloc("World!").unwrap();
+        assert_eq!(unsafe { ptr2.read() }, "World!");
+
+        assert_eq!(ptr1.as_ptr() as usize, ptr2.as_ptr() as usize);
     }
 }
--- src/memory/allocators/general.rs
+++ src/memory/allocators/general.rs
@@ -1,6 +1,17 @@
 // A memory manager manages a block of memory as a heap
 
+use std::{alloc::{Layout, dealloc}, ptr::NonNull};
+
 pub struct GeneralAllocator
 {
+    base: NonNull<u8>,
+    layout: Layout
+}
 
+impl Drop for GeneralAllocator
+{
+    fn drop(&mut self)
+    {
+        unsafe { dealloc(self.base.as_ptr(), self.layout); }
+    }
 }
