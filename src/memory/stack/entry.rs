fd10741645fcd2a7158948ab6b3dc68daf9f9c7e
Sophie Holman <siaphix.siaphix@gmail.com>
2026-02-02 15:19:09 +0000

    stack entry mapping and binary operators

--- src/memory/stack/entry.rs
+++ src/memory/stack/entry.rs
@@ -11,3 +11,27 @@ pub enum StackEntry
     Double(f64),
     Reference(Option<NonNull<u8>>)
 }
+
+impl StackEntry
+{
+    fn try_binary_operation<T, F>(&self, other: &Self, op: F) -> Option<Self>
+    where
+        Self: TryInto<T>,
+        T: Into<StackEntry>,
+        F: Fn(T, T) -> T
+    {
+        let first = self.try_into().ok()?;
+        let second = other.try_into().ok()?;
+
+        Some(op(first, second).into())
+    }
+
+    fn try_map<T, F>(&self, op: F) -> Option<Self>
+    where
+        Self: TryInto<T>,
+        T: Into<StackEntry>,
+        F: Fn(T) -> T
+    {
+        Some(op(self.try_into().ok()?).into())
+    }
+}
--- src/memory/stack/stackable.rs
+++ src/memory/stack/stackable.rs
@@ -1,12 +1,27 @@
 use std::ptr::NonNull;
-use crate::memory::stack::entry::StackEntry;
+use crate::memory::stack::{entry::StackEntry};
 
+struct EntryExtractionError;
 macro_rules! impl_into_entry {
     ($($t:ty => $r:tt),+) => {
         $(
             impl From<$t> for StackEntry
             {
                 fn from(value: $t) -> Self { Self::$r(value) }
+            }
+
+            impl TryFrom<StackEntry> for $t
+            {
+                type Error = EntryExtractionError;
+
+                fn try_from(value: StackEntry) -> Result<Self, Self::Error>
+                {
+                    match value
+                    {
+                        StackEntry::$r(x) => Ok(x),
+                        _ => Err(EntryExtractionError)
+                    }
+                }
             }
         )+
     };
